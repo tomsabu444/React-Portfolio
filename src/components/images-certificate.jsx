414c2ad73d0feaf3a7a4043f2811b52e5c3d3002
TOM SABU <tomsabu0000@gmail.com>
2024-01-03 08:40:16 +0530

    Revert "Certificate require plugin --fixed V 0.3"
    This reverts commit a0d286a033d2911fcec898c1cfcefb54644f9e86.

--- src/components/images-certificate.jsx
+++ src/components/images-certificate.jsx
@@ -1,14 +1,14 @@
 const images = {
-    Dbms_Nptel_Certificate: require('../Error-handling/ImageNotFound.gif'),
-    Offenso_certificate: require('../Error-handling/ImageNotFound.gif'),
-    TKM_certificate: require('../Error-handling/ImageNotFound.gif'),
-    IIT_Madras_certificate1: require('../Error-handling/ImageNotFound.gif'),
-    Blender_linkedin_certificate: require('../Error-handling/ImageNotFound.gif'),
-    Linux_linkedin_certificate: require('../Error-handling/ImageNotFound.gif'),
-    IIT_Madras_certificate2: require('../Error-handling/ImageNotFound.gif'),
-    Wireless_network_linkedin_certificate: require('../Error-handling/ImageNotFound.gif'),
-    GameIndustry_linkedin_certificate: require('../Error-handling/ImageNotFound.gif'),
-    Tegmagi_Intership_certificate: require('../Error-handling/ImageNotFound.gif'),
+    // Dbms_Nptel_Certificate: require('../assets/images/Data_Base_Management_System-1.webp'),
+    // Offenso_certificate: require('../assets/images/TOM_SABU_page-0001.webp'),
+    // TKM_certificate: require('../assets/images/cr_5.webp'),
+    // IIT_Madras_certificate1: require('../assets/images/cr_3-1.webp'),
+    // Blender_linkedin_certificate: require('../assets/images/CertificateOfCompletion_Blender_3_Essential_Training_page-0001.webp'),
+    // Linux_linkedin_certificate: require('../assets/images/CertificateOfCompletion_Linux_Overview_and_Installation_page-0001.webp'),
+    // IIT_Madras_certificate2: require('../assets/images/cr_4-1.webp'),
+    // Wireless_network_linkedin_certificate: require('../assets/images/cr_1.webp'),
+    // GameIndustry_linkedin_certificate: require('../assets/images/cr_2.webp'),
+    // Tegmagi_Intership_certificate: require('../assets/images/TOM_SABU_1_page-0001.webp'),
   };
 
   export default images
