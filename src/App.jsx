1ce19e1d3d3ffc35a3d7ac5e54886944c3204f23
TOM SABU <tomsabu0000@gmail.com>
2024-01-05 20:25:51 +0530

    HashRouter --updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React from 'react';
-import {  HashRouter as Router , Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
+import React from "react";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 // import Certificate from './pages/Certificate'
-import Projects from './pages/Projects';
-import Resume from './pages/Resume';
-import Maintenance from './components/Maintenance';
-import Error404 from "./Error-handling/404-Error"
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
+import Maintenance from "./components/Maintenance";
+import Error404 from "./Error-handling/404-Error";
 function App() {
   return (
     <>
-
       <div className="dev-mod">
-
         <Router>
           <Routes>
             <Route exact path="/" element={<Home />} />
@@ -21,17 +19,13 @@ function App() {
 
             {/* Route for 404 Not Found */}
             <Route path="*" element={<Error404 />} />
-            
           </Routes>
         </Router>
-
       </div>
 
-        {/* <Maintenance/>  */}
-
-
+      {/* <Maintenance/>  */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
