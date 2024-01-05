87c7d949168763ea425dda5f40c81d61a27deb3e
TOM SABU <tomsabu0000@gmail.com>
2024-01-05 21:57:17 +0530

    Skill-icon --changed_to_array

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 import styled from "styled-components";
 import Layout from "../components/Layout";
+import { tools_Icons_1, tools_Icons_2 } from "../assets/Skill-Icon";
 
 function Home() {
+  // console.log(tools_Icons_1);
   return (
     <Layout>
       <Container>
@@ -25,412 +27,43 @@ function Home() {
             </p>
           </section>
 
-  {/*        <section className="service">
-            <h3 className="h3 service-title"> Tools i'm Using!</h3>
+          {/* Horizontal Scrooll 1 */}
 
-            <ul className="service-list">
-               <li className="service-item">
-                <div className="service-content-box">
-                  <div className="service-item-text">
-                    <img
-                      draggable="false"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"
-                      alt="figma"
-                      title="Figma"
-                      width="40"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
-                      alt="HTML"
-                      title="HTML"
-                      width="40"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-                      alt="CSS"
-                      title="CSS"
-                      width="40"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://skillicons.dev/icons?i=js"
-                      alt="Java Script"
-                      title="Java Script"
-                      width="40"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://skillicons.dev/icons?i=react"
-                      height="40"
-                      alt="react logo"
-                      title="React"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://skillicons.dev/icons?i=bootstrap"
-                      alt="Bootstrap"
-                      title="Bootstrap"
-                      width="40"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://skillicons.dev/icons?i=php"
-                      alt="PHP"
-                      title="PHP"
-                      width="40"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://cdn.simpleicons.org/apache/D22128"
-                      height="40"
-                      title="Apache"
-                      alt="apache logo"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://cdn.simpleicons.org/mysql/4479A1"
-                      height="40"
-                      title="MySQL"
-                      alt="mysql logo"
-                    />
-                  </div>
-                </div>
-              </li> */}
-              {/* 
-              <li className="service-item">
-                <div className="service-content-box">
-                  <div className="service-item-text">
-                    <img
-                      draggable="false"
-                      src="https://skillicons.dev/icons?i=flutter"
-                      height="40"
-                      alt="flutter logo"
-                      title="Flutter"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://skillicons.dev/icons?i=dart"
-                      height="40"
-                      alt="dart logo"
-                      title="Dart"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg"
-                      height="40"
-                      alt="androidstudio logo"
-                      title="Android Studio"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://skillicons.dev/icons?i=nodejs"
-                      height="40"
-                      alt="nodejs logo"
-                      title="Nodejs"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://cdn.simpleicons.org/android/3DDC84"
-                      height="40"
-                      alt="android logo"
-                      title="Android"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://skillicons.dev/icons?i=git"
-                      height="40"
-                      alt="git logo"
-                      title="Git"
-                    />
-                  </div>
-                </div>
-              </li> */}
-{/* 
-              <li className="service-item">
-                <div className="service-content-box">
-                  <div className="service-item-text">
-                    <img
-                      src="https://skillicons.dev/icons?i=ae"
-                      height="40"
-                      alt="adobeaftereffects logo"
-                      title="Adobe After Effects "
-                    />
-                    <img
-                      src="https://skillicons.dev/icons?i=pr"
-                      height="40"
-                      alt="adobepremierepro logo"
-                      title="Adobe Premiere Pro"
-                    />
-                    <img
-                      src="https://skillicons.dev/icons?i=ps"
-                      height="40"
-                      alt="adobephotoshop logo"
-                      title="Adobe Photoshop"
-                    />
-                    <img
-                      src="https://skillicons.dev/icons?i=ai"
-                      height="40"
-                      alt="adobeillustrator logo"
-                      title="Adobe Illustrator"
-                    />
-                    <img
-                      src="https://skillicons.dev/icons?i=blender"
-                      height="40"
-                      alt="blender logo"
-                      title="Blender"
-                    />
-                    <img
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/maya/maya-original.svg"
-                      height="40"
-                      alt="maya logo"
-                      title="Maya"
-                    />
-                  </div>
-                </div>
-              </li>
+          <section className="tool-icon">
+            <h3 className="h3 tool-icon-title">Tools I'm Using!</h3>
 
-              <li className="service-item">
-                <div className="service-content-box">
-                  <div className="service-item-text">
-                    <img
-                      draggable="false"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg"
-                      height="40"
-                      alt="vscode logo"
-                      title="Visual Studio Code"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://skillicons.dev/icons?i=discord"
-                      height="40"
-                      alt="discord logo"
-                      title="Discord"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/googlecloud/googlecloud-original.svg"
-                      height="40"
-                      alt="googlecloud logo"
-                      title="Google Cloud"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://cdn.simpleicons.org/c/A8B9CC"
-                      height="40"
-                      alt="c logo"
-                      title="C Programing"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://skillicons.dev/icons?i=java"
-                      height="40"
-                      alt="java logo"
-                      title="Java"
-                    />
-                    <img
-                      draggable="false"
-                      src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg"
-                      height="40"
-                      alt="python logo"
-                      title="Python"
-                    />
-                  </div>
-                </div>
-              </li>
-            </ul>
-          </section> */}
-
-          <section className="clients">
-            <h3 className="h3 clients-title">Tools I'm Using!</h3>
-
-            <ul className="clients-list has-scrollbar">
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/figma/figma-original.svg"
-                  alt="figma"
-                  title="Figma"
-                  height="50"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg"
-                  alt="HTML"
-                  title="HTML"
-                  height="50"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg"
-                  alt="CSS"
-                  title="CSS"
-                  height="50"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://skillicons.dev/icons?i=js"
-                  alt="Java Script"
-                  title="Java Script"
-                  height="50"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://skillicons.dev/icons?i=react"
-                  height="50"
-                  alt="react logo"
-                  title="React"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://skillicons.dev/icons?i=bootstrap"
-                  alt="Bootstrap"
-                  title="Bootstrap"
-                  height="50"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://skillicons.dev/icons?i=php"
-                  alt="PHP"
-                  title="PHP"
-                  height="50"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://cdn.simpleicons.org/apache/D22128"
-                  height="50"
-                  title="Apache"
-                  alt="apache logo"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://cdn.simpleicons.org/mysql/4479A1"
-                  height="50"
-                  title="MySQL"
-                  alt="mysql logo"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://skillicons.dev/icons?i=flutter"
-                  height="40"
-                  alt="flutter logo"
-                  title="Flutter"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://skillicons.dev/icons?i=dart"
-                  height="40"
-                  alt="dart logo"
-                  title="Dart"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/androidstudio/androidstudio-original.svg"
-                  height="40"
-                  alt="androidstudio logo"
-                  title="Android Studio"
-                />
-              </li>
-              <li className="clients-item">
-
-              <img
-                draggable="false"
-                src="https://skillicons.dev/icons?i=nodejs"
-                height="40"
-                alt="nodejs logo"
-                title="Nodejs"
-                />
+            <ul className="tool-icon-list has-scrollbar">
+              {tools_Icons_1.map((icon, index) => (
+                <li className="tool-icon-item">
+                  <img
+                    draggable="false"
+                    src={icon.src}
+                    alt={icon.alt}
+                    title={icon.title}
+                    height={icon.height}
+                  />
                 </li>
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://cdn.simpleicons.org/android/3DDC84"
-                  height="40"
-                  alt="android logo"
-                  title="Android"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  draggable="false"
-                  src="https://skillicons.dev/icons?i=git"
-                  height="40"
-                  alt="git logo"
-                  title="Git"
-                />
-              </li>
+              ))}
+            </ul>
+          </section>
 
-              <li className="clients-item">
-                <img
-                  src="https://skillicons.dev/icons?i=ae"
-                  height="40"
-                  alt="adobeaftereffects logo"
-                  title="Adobe After Effects "
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  src="https://skillicons.dev/icons?i=pr"
-                  height="40"
-                  alt="adobepremierepro logo"
-                  title="Adobe Premiere Pro"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  src="https://skillicons.dev/icons?i=ps"
-                  height="40"
-                  alt="adobephotoshop logo"
-                  title="Adobe Photoshop"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  src="https://skillicons.dev/icons?i=ai"
-                  height="40"
-                  alt="adobeillustrator logo"
-                  title="Adobe Illustrator"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  src="https://skillicons.dev/icons?i=blender"
-                  height="40"
-                  alt="blender logo"
-                  title="Blender"
-                />
-              </li>
-              <li className="clients-item">
-                <img
-                  src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/maya/maya-original.svg"
-                  height="40"
-                  alt="maya logo"
-                  title="Maya"
-                />
-              </li>
+          {/* Horizontal Scrooll 2*/}
+
+          <section className="tool-icon">
+            <h3 className="h3 tool-icon-title">Software I'm Using!</h3>
+
+            <ul className="tool-icon-list has-scrollbar">
+              {tools_Icons_2.map((icon, index) => (
+                <li className="tool-icon-item">
+                  <img
+                    draggable="false"
+                    src={icon.src}
+                    alt={icon.alt}
+                    title={icon.title}
+                    height={icon.height}
+                  />
+                </li>
+              ))}
             </ul>
           </section>
         </article>
@@ -520,14 +153,18 @@ const Container = styled.div`
   }
 
   /**
- * #clients 
+ * #tool-icon 
  */
 
-  .clients {
-    margin-bottom: 15px;
+  .tool-icon {
+    margin-bottom: 35px;
   }
 
-  .clients-list {
+  .tool-icon-title {
+    text-align: center;
+  }
+
+  .tool-icon-list {
     display: flex;
     justify-content: flex-start;
     align-items: flex-start;
@@ -542,18 +179,18 @@ const Container = styled.div`
     scroll-padding-inline: 25px;
   }
 
-  .clients-item {
+  .tool-icon-item {
     min-width: 50%;
     scroll-snap-align: start;
   }
 
-  /* .clients-item img {
+  /* .tool-icon-item img {
     width: 100%;
     filter: grayscale(1);
     transition: var(--transition-1);
   }
 
-  .clients-item img:hover {
+  .tool-icon-item img:hover {
     filter: grayscale(0);
   } */
 
@@ -570,7 +207,7 @@ const Container = styled.div`
    * client
    */
 
-    .clients-item {
+    .tool-icon-item {
       min-width: calc(33.33% - 10px);
     }
   }
@@ -617,17 +254,17 @@ const Container = styled.div`
     }
 
     /*
-     clients
+     tool-icon
       */
 
-    .clients-list {
+    .tool-icon-list {
       gap: 50px;
 
       padding: 45px;
       scroll-padding-inline: 45px;
     }
 
-    .clients-item {
+    .tool-icon-item {
       min-width: calc(33.33% - 35px);
     }
   }
@@ -666,10 +303,10 @@ const Container = styled.div`
     }
 
     /*
-     clients
+     tool-icon
      */
 
-    .clients-item {
+    .tool-icon-item {
       min-width: calc(20% - 40px);
     }
   }
