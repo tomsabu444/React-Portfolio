4b39a0d55130b99392c95852b26370f94afcd445
Tom Sabu <tomsabu0000@gmail.com>
2026-08-03 22:16:07 +0530

    feat: add framer-motion for animations, create Badge component, and enhance TimelinePage with experience details

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import Layout from "@/layout/Layout";
 import AboutPage from "@/pages/AboutPage";
-import ContactPage from "@/pages/ContactPage";
 import HomePage from "@/pages/HomePage";
-import ProjectsPage from "@/pages/ProjectsPage";
 import TimelinePage from "@/pages/TimelinePage";
 
 function App() {
@@ -10,8 +8,8 @@ function App() {
     <Layout>
       <HomePage />
       <AboutPage />
-      {/* <TimelinePage />
-      <ProjectsPage />
+      <TimelinePage />
+      {/* <ProjectsPage />
       <ContactPage /> */}
     </Layout>
   );
